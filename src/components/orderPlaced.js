16dc52a91fe2a3d645f60005b94b06e41c14eeac
Ravi <Ravi.rr2015rs@gmail.com>
2020-04-16 12:29:04 +0530

    project

---
+++ src/components/orderPlaced.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Order extends Component {
+    render(){
+        return (
+            <div className="container mt-5">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-title text-capitalize">
+                  <h1>Thank You</h1>
+                  <h2>for placing the order</h2>
+                </div>
+              </div>
+            </div>
+        
+    )
+        }
+    
+  
+}
